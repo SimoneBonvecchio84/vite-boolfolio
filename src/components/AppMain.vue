eddf0db09157038e3ff8d2dc3ab7d2f965bffd75
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-06-30 18:29:10 +0200

    Feat: Print curProjet in appMain with v-for

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,36 @@
 <script>
+import axios from 'axios';
 export default {
-    
+    data() {
+        return {
+            projects: []
+        }
+    },
+    created() {
+        axios.get('http://127.0.0.1:8000/api/projects').then((resp) => {
+            console.log(resp);
+            this.projects = resp.data.result;
+        })
+    }
 }
 </script>
 
 <template>
-<h1>Ciao</h1>
+    <div class="container">
+        <h1>Lista Progetti</h1>
+        <div class="row">
+            <div class="col" v-for="curProject in projects ">
+                <div class="card">
+                    <h4>Titolo</h4>
+                    {{ curProject.title }}
+                    <h5>Tipologia</h5>
+                    {{ curProject.type_id }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
