e15d098e1a252dfc6d34e5a1feade9f1db0c42ac
SimoneBonvecchio84 <157390564+SimoneBonvecchio84@users.noreply.github.com>
2024-06-30 17:49:51 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+  import AppTitleImg from  './components/AppTitleImg.vue'
+
+  export default {
+  components: {
+    AppTitleImg
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  
+    <h1>Ciao Mondo</h1>
+
+ 
+  <section>
+    <AppTitleImg />
+  </section>
+</template>
+
+<style>
+ 
+</style>
---
+++ src/components/AppTitleImg.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    data () {
+        return {
+            title: "Simone"
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <h2>{{ title }}</h2>
+        <img src="../assets/img/simo.jpg" alt="">
+    </div>
+
+</template>
+
+<style>
+    h2 {
+       position: absolute;
+       color:white;
+       top: 50%;
+       left: 50%;
+       transform: translate(-50%, -50%);
+       font-size: 3rem;
+    }
+    .container {
+        position: relative;
+
+    }
+    .container img {
+        width: 100%;
+    }
+</style>
